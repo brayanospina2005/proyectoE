df58193a97f16f3758fc9582b6284b78dde990e2
Sandy Ruiz <sadrh97@gmail.com>
2023-02-06 19:05:11 -0500

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { MainComponent } from './main/main.component';
-import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './header/header.component';
-import { SectionCursosComponent } from './main/section-cursos/section-cursos.component';
+import { FormsModule } from '@angular/forms';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LayoutComponent } from './layout/layout.component';
 import { CursosComponent } from './main/cursos/cursos.component';
 import { DescripcionComponent } from './main/cursos/descripcion/descripcion.component';
 
@@ -16,17 +15,16 @@ import { DescripcionComponent } from './main/cursos/descripcion/descripcion.comp
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    FooterComponent,
     LoginComponent,
-    HeaderComponent,
-    SectionCursosComponent,
     CursosComponent,
     DescripcionComponent,
+    PageNotFoundComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
