cdae93955672c099064e9a6f09ce86b0f39bf7ef
Sandy Ruiz <sadrh97@gmail.com>
2023-02-16 19:16:14 -0500

    Cursos services e interface

--- src/app/curso.service.ts
+++ src/app/curso.service.ts
@@ -6,11 +6,64 @@ import { cursosInfo } from './main/section-cursos/section-cursos.component';
 })
 export class CursoService {
 
-  cursosColeccion: cursosInfo[] = [];
+  cursos: cursosInfo[] = [
+    {
+      title: 'Yoga',
+      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
+      descripcion: 'inscribete o lloro',
+      id: '1',
+    },{
+      title: 'Yoga',
+      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
+      descripcion: 'inscribete o lloro',
+      id: '2',
+    },{
+      title: 'Yoga',
+      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
+      descripcion: 'inscribete o lloro',
+      id: '3',
+    },{
+      title: 'Yoga',
+      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
+      descripcion: 'inscribete o lloro',
+      id: '4',
+    },{
+      title: 'Yoga',
+      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
+      descripcion: 'inscribete o lloro',
+      id: '5',
+    },{
+      title: 'Yoga',
+      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
+      descripcion: 'inscribete o lloro',
+      id: '6',
+    },{
+      title: 'Yoga',
+      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
+      descripcion: 'inscribete o lloro',
+      id: '7',
+    },{
+      title: 'Yoga',
+      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
+      descripcion: 'inscribete o lloro',
+      id: '8',
+    },
+  ];
 
   constructor() { };
 
+
+  getCursos(){
+    return this.cursos;
+  }
+
+  setCursos(cursos: cursosInfo[]) {
+    this.cursos = cursos;
+  }
+
   agregarCurso(curso: cursosInfo) {
-    this.cursosColeccion.push(curso);
+    this.cursos.push(curso);
+    const cursosActuales = this.getCursos();
+    this.setCursos(cursosActuales);
   };
 }
--- src/app/main/section-cursos/section-cursos.component.ts
+++ src/app/main/section-cursos/section-cursos.component.ts
@@ -7,60 +7,15 @@ import { CursoService } from 'src/app/curso.service';
   styleUrls: ['./section-cursos.component.css']
 })
 export class SectionCursosComponent {
+  cursos: cursosInfo[] =[];
 
-  CursosColeccion: cursosInfo[] = [
-    {
-      title: 'Yoga',
-      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
-      descripcion: 'inscribete o lloro',
-      id: '1',
-    },{
-      title: 'Yoga',
-      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
-      descripcion: 'inscribete o lloro',
-      id: '2',
-    },{
-      title: 'Yoga',
-      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
-      descripcion: 'inscribete o lloro',
-      id: '3',
-    },{
-      title: 'Yoga',
-      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
-      descripcion: 'inscribete o lloro',
-      id: '4',
-    },{
-      title: 'Yoga',
-      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
-      descripcion: 'inscribete o lloro',
-      id: '5',
-    },{
-      title: 'Yoga',
-      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
-      descripcion: 'inscribete o lloro',
-      id: '6',
-    },{
-      title: 'Yoga',
-      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
-      descripcion: 'inscribete o lloro',
-      id: '7',
-    },{
-      title: 'Yoga',
-      Image: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/o-que-e-hateoas-400x280.png',
-      descripcion: 'inscribete o lloro',
-      id: '8',
-    },
-  ];
 
-  constructor(private cursoService: CursoService) { }
-
-  // onCursoCreado(nuevoCurso: cursosInfo) {
-  //   this.CursosColeccion.push(nuevoCurso); // Agrega el nuevo curso a la colección de cursos
-  // }
+  constructor(private cursoService: CursoService) {
+    this.cursos = this.cursoService.getCursos();
+   }
   
   agregarCurso(nuevoCurso: cursosInfo) {
-    this.CursosColeccion.push(nuevoCurso);
-    console.log(this.CursosColeccion);
+    this.cursoService.agregarCurso(nuevoCurso);
   }
 
 }
