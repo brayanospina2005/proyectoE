4e1f4d527f43afbaf57aa7ac78014e2146b79b81
Sandy Ruiz <sadrh97@gmail.com>
2023-02-18 23:40:42 -0500

    Imagen predeterminada para nuevo curso

--- src/app/curso.service.ts
+++ src/app/curso.service.ts
@@ -80,6 +80,8 @@ export class CursoService {
     },
   ];
 
+  defaultImageUrl = "https://images.unsplash.com/photo-1543858828-7cf1a9beb95c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
+
   constructor() {}
 
   getCursos() {
@@ -91,6 +93,7 @@ export class CursoService {
   }
 
   agregarCurso(Curso: cursosInfo) {
+    Curso.Image = this.defaultImageUrl;
     this.cursos.push(Curso);
   }
 }
